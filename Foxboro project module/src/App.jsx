c89a47ac39d994d7ab09f4ea9b2e107ca89216ec
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2025-01-24 11:23:49 +0530

    routing set

--- Foxboro project module/src/App.jsx
+++ Foxboro project module/src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 import User from "./pages/UserProfile/userProfile";
-import Forgot from './pages/Forgot/forgot';
+import Forgot from "./pages/Forgot/forgot";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import ResetPassword from './pages/Forgot/resetPassword';
-function App(){
+import ResetPassword from "./pages/Forgot/resetPassword";
+function App() {
   return (
     <div>
       {/* <User/> */}
       {/* <Forgot/> */}
       <BrowserRouter>
-      <Routes>
-        <Route path='/reset' element=<ResetPassword/> />
-        <Route path='/forgot' element=<Forgot/> />
-      </Routes>
+        <Routes>
+          <Route path="/reset" element=<ResetPassword /> />
+          <Route path="/forgot" element=<Forgot /> />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
